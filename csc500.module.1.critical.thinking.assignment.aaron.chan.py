7e11c1463a408ddd31cb16414c21553ea2612a9e
AC @ GitHub <aaron.chan@csuglobal.edu>
2025-01-19 10:24:57 -0500

    Update csc500.module.1.critical.thinking.assignment.aaron.chan.py

--- csc500.module.1.critical.thinking.assignment.aaron.chan.py
+++ csc500.module.1.critical.thinking.assignment.aaron.chan.py
@@ -16,39 +16,58 @@
 # Also, divide num1/num2 to find the output.
 #
 # ------------------------------------------------
-# Not In Scope:
-# - This version focuses on demonstrating basic input capture, arithmatics (addition & substraction) and display, per assignment requirements,
-#   and does not handle validation such as data types verification, etc.
-#
-# ------------------------------------------------
 # Modification Log:
 # - 1/16/2025 Initiated.
 # - 1/19/2025 Add code comments.
+# - 1/19/2025 Refactoring some codes to methods all part of modularization effort.
 # ------------------------------------------------
 
+# ------------------------------------------------
+# Method to capture and return a number representing input from end users via command (e.g.: Shell command) prompt.
+# ------------------------------------------------
+def get_number_input(prompt_text):
+    try:
+        input_value = int(input(prompt_text))
+    except ValueError:
+        print("Invalid value. An integer is expected.")
+    except Exception as e:
+        print("Unexpected error has occured.")
+
+    return input_value
 
 # ------------------------------------------------
-# Input capture of variable num1 & num2 from End Users via command (e.g.: Shell command) prompt.
+# Method containing the main program flows.
 # ------------------------------------------------
+def main():
+    # ------------------------------------------------
+    # Input capture of variable num1 & num2 handled by a separate method.
+    # ------------------------------------------------
 
-num1 = input("\nEnter first number: ")
-num2 = input("Enter second number: ")
+    num1 = get_number_input("\nEnter a number for num1: ")
+    num2 = get_number_input("\nEnter a number for num2: ")
 
-# ------------------------------------------------
-# Displaying the captured input values.
-# ------------------------------------------------
+    # additional handling of num2, which must not be a zero (to prevent division by zero error)
+    if num2 == 0:
+        raise ValueError("num2 cannot be 0.") 
 
-print("\nYou've entered num1 =", num1, ", num2 =", num2)
+    # ------------------------------------------------
+    # Displaying the captured input values.
+    # ------------------------------------------------
+
+    print("\nYou've entered num1 =", num1, ", num2 =", num2)
 
 
-# ------------------------------------------------
-# Displaying the output values, which also involves performing basic arithmatic operations to obtain the output values.
-# ------------------------------------------------
+    # ------------------------------------------------
+    # Displaying the output values, which also involves performing basic arithmatic operations to obtain the output values.
+    # ------------------------------------------------
 
-print("\nThe sum of num1 and num2 is", num1, "+", num2, "=", int(num1) + int(num2))
+    print("\nThe sum of num1 and num2 is", num1, "+", num2, "=", int(num1) + int(num2))
 
-print("\nThe result of num1 minus num2 is", num1, "-", num2, "=", int(num1) - int(num2))
+    print("\nThe result of num1 minus num2 is", num1, "-", num2, "=", int(num1) - int(num2))
 
-print("\nThe result of num1 divided by num2 is", num1, "/", num2, "=", int(num1) / int(num2))
+    print("\nThe result of num1 divided by num2 is", num1, "/", num2, "=", int(num1) / int(num2))
 
+
+# program main construct
+if __name__ ==  '__main__': main()
 
